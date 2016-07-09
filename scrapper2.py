936276b01281b44da1e2ea105698cc107bd8aa24
Lennox" Shou Hao Ho <lennox.ho@mail.utoronto.ca>
2016-07-09 13:12:29 -0400

    Fixed colour printing

--- scrapper2.py
+++ scrapper2.py
@@ -1,4 +1,5 @@
 import sys
+import lib2.colorama.initialise; lib2.colorama.initialise.init()
 
 if sys.version_info[0] < 3:
     # Use Python 2 compliant printing style
@@ -9,8 +10,6 @@ if sys.version_info[0] < 3:
 from lib2.scrapper2_core import *
 
 if __name__ == "__main__":
-    import lib2.colorama.initialise; lib2.colorama.initialise.init()
-
     error_out("This Python module acts as the common interface for scrapper2. " + \
               "Please import this module into your script to use its functionality.")
     sys.exit(1)
